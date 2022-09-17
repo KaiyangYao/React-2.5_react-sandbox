21231730c45d5b61246807bfdd613b116540b63c
Kaiyang Yao <yaoky2008@gmail.com>
2022-09-16 20:57:00 -0500

    add useMemo hook example

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from './components/Home'
 import UseRefExample1 from './components/UseRefExample1'
 import UseRefExample2 from './components/UseRefExample2'
 import UseRefExample3 from './components/UseRefExample3'
-
+import UseMemoExample from './components/UseMemoExample'
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
           <Route path='/useref-example-1' element={<UseRefExample1 />} />
           <Route path='/useref-example-2' element={<UseRefExample2 />} />
           <Route path='/useref-example-3' element={<UseRefExample3 />} />
+          <Route path='/usememo-example' element={<UseMemoExample />} />
         </Routes>
       </div>
     </Router>
